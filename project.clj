f14644dee7bf668dbee6b0f9790a5152de4f7808
Kyle Kingsbury <kingsbury@factual.com>
2013-06-12 15:44:37 -0700

    Project description, site, and ojo dep

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject prism "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Re-run lein tests automatically"
+  :url "http://github.com/aphyr/prism"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"
+                  ojo "1.1.0"]])
